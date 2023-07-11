bc480e5d60ca31856750b258e51d445adf7c3065
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2023-07-11 09:32:41 +0545

    production base url

--- src/config/apiConfig.jsx
+++ src/config/apiConfig.jsx
@@ -1,3 +1,5 @@
-const API_BASE_URL = 'http://127.0.0.1:3001';
+//const API_BASE_URL = 'http://127.0.0.1:3001';
+const API_BASE_URL = 'https://tranquil-springs-32726-3204834d8936.herokuapp.com/';
+
 
 export default API_BASE_URL;

7818a424f8dc05a963dc3f34a5e6bceaad8ccf55
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2023-10-29 00:57:59 +0200

    update to google maps config

--- src/components/Requests/MakeRequestForm.jsx
+++ src/components/Requests/MakeRequestForm.jsx
@@ -102,7 +102,7 @@ const MakeRequestForm = ({ setModalOn }) => {
                   <div className='mb-3'>
                     <GooglePlacesAutocomplete
                       required
-                      apiKey="AIzaSyCE7PIXaFYEytgui9s35o-UoOAlCptX4y4"
+                      apiKey="AIzaSyCZ0M7Vq0tuaUL7LrGd7ViN2iYl2YUZ96I"
                       selectProps={{
                         onChange: (address) => setAddress(address.label),
                       }}

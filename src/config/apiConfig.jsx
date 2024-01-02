8d2c2bffae425e39a17368444d8efc45e4efb077
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2024-01-03 00:38:46 +0100

    api config for development

--- src/config/apiConfig.jsx
+++ src/config/apiConfig.jsx
@@ -1,5 +1,4 @@
-//const API_BASE_URL = 'http://127.0.0.1:3001';
-const API_BASE_URL = 'https://tranquil-springs-32726-3204834d8936.herokuapp.com';
+const API_BASE_URL = 'http://127.0.0.1:3001';
 
 
 export default API_BASE_URL;
